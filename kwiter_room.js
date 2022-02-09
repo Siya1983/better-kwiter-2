6b557f23fe48da31f08c396516d7df5091020fcd
Siya1983 <94732111+Siya1983@users.noreply.github.com>
2022-02-09 08:25:59 +0530

    Add files via upload

---
+++ kwiter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8W3x5z5ug7I_T1Qqk9Jgv_xp_haiDaUI",
+    authDomain: "better-kwitter.firebaseapp.com",
+    databaseURL: "https://better-kwitter-default-rtdb.firebaseio.com",
+    projectId: "better-kwitter",
+    storageBucket: "better-kwitter.appspot.com",
+    messagingSenderId: "646301911073",
+    appId: "1:646301911073:web:42d32a5d8ebbf148e33b60"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
